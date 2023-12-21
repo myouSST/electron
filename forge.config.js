ff155dead9fece1987a8b7d826885b18117e465c
유민 <myou@spectra.co.kr>
2023-12-21 09:26:17 +0900

    setup 파일 작업

--- forge.config.js
+++ forge.config.js
@@ -1,39 +1,51 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        iconUrl: 'https://dworks-workspace.spectra.co.kr/btalk/favicon.ico',
-        description: 'Messaging Works Platform',
-      },
+    packagerConfig: {
+        asar: true,
+        icon: './electron/icons'
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        icon: './electron/icons/app.icns',
-      }
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                iconUrl: 'https://dworks-workspace.spectra.co.kr/btalk/favicon.ico',
+                description: 'Messaging Works Platform',
+                setupIcon: './electron/icons/app.ico',
+            },
+        },
+        {
+            name: '@electron-forge/maker-wix',
+            config: {
+                language: 1033,
+                manufacturer: 'My Awesome Company',
+                ui: {
+                    chooseDirectory: true
+                }
+            }
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-dmg',
+            config: {
+                icon: './electron/icons/app.icns',
+            }
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+    ],
 };
